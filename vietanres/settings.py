ac55b8910c350e38072ac7b89b8796f446cfc94a
Huy Chau <huy.chau@besmartee.com>
2024-05-21 18:37:01 +0700

    Build
    Update settings.py
    
    Update settings.py
    
    Fix static not loading
    
    Update settings.py

--- vietanres/settings.py
+++ vietanres/settings.py
@@ -37,7 +37,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-j=z_w$hs&p(^bfp6rzkxp7e@52akejq72d37@d1h1_crb+av-j"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["51.195.101.225", "127.0.0.1", "viet-an.co.uk", "vietan.phapsuit.com","localhost", ".vercel.app", '.now.sh']
 
@@ -152,9 +152,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
+STATIC_URL = "/static/"
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
