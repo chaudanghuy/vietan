781bc739a6375208f2e9ea75d2eb37ca8a5b8c7d
Huy Chau <huy.chau@besmartee.com>
2024-05-21 16:11:19 +0700

    Update settings.py
    Update requirements.txt

--- vietanres/settings.py
+++ vietanres/settings.py
@@ -50,8 +50,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "django_recaptcha",
+    "django.contrib.staticfiles",    
     "apps.restaurant",    
 ]
 
