03cf1c5bb711092e373a3fe23eaba053ae3768fc
Huy Chau <huy.chau@besmartee.com>
2024-05-22 12:21:01 +0700

    Update settings.py

--- vietanres/settings.py
+++ vietanres/settings.py
@@ -112,8 +112,12 @@ elif DATABASE_TYPE == "postgres":
 else:
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",            
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'railway',
+            'USER': 'postgres',
+            'PASSWORD': 'LwpsggBKmlGSRDbTJIrpjNeqZfajEFTc',
+            'HOST': 'viaduct.proxy.rlwy.net',
+            'PORT': '40117',      
         }
     }
 
