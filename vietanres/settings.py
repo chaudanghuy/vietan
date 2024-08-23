9f052b3ce32991fc15d24ed3faffb574759e8be3
Huy Chau <phimmuot@gmail.com>
2024-08-23 21:38:50 +0700

    Update settings.py

--- vietanres/settings.py
+++ vietanres/settings.py
@@ -65,7 +65,7 @@ SECRET_KEY = "django-insecure-j=z_w$hs&p(^bfp6rzkxp7e@52akejq72d37@d1h1_crb+av-j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (ENV == 'Local')
 
-ALLOWED_HOSTS = ["51.195.101.225", "127.0.0.1", "viet-an.co.uk", "vietan.phapsuit.com","localhost", ".vercel.app", '.now.sh']
+ALLOWED_HOSTS = ["51.195.101.225", "127.0.0.1", "viet-an.co.uk", "vietan.phapsuit.com","localhost", ".vercel.app", '.now.sh', '65.20.73.185']
 
 
 # Application definition
